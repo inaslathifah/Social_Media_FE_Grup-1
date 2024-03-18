edcdd37de0b86fea18fbd6ef1397f68118470c46
adeprastyo <adeprastyo101@gmail.com>
2024-03-18 21:45:10 +0700

    init project: install react vite, tailwindcss, and shadcnui

---
+++ src/pages/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "../components/ui/button";
+
+function App() {
+  return (
+    <div className="w-full h-dvh flex flex-col justify-center items-center bg-sky-700">
+      <p className="text-lg text-white">Hello World</p>
+      <Button>Click</Button>
+    </div>
+  );
+}
+
+export default App;
