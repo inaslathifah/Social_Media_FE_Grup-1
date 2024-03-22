5056585cffa7d7d3454c28a426eddf9df7fc684d
Yoga S Devanada <ydevanada@gmail.com>
2024-03-22 11:37:05 +0700

    feat: add reusable alert

---
+++ src/components/alert.tsx
@@ -0,0 +1,44 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+
+interface Props {
+  open: boolean;
+  title: string;
+  description: string;
+  onOpenChange?: (open: boolean) => void;
+  onCancel?: () => void;
+  onAction?: () => void;
+}
+
+const Alert = (props: Props) => {
+  const { open, description, onOpenChange, onAction, onCancel, title } = props;
+
+  return (
+    <AlertDialog open={open} onOpenChange={onOpenChange}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel data-testid="alert-cancel" onClick={onCancel}>
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction data-testid="alert-yes" onClick={onAction}>
+            Yes
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default Alert;
